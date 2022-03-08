b033fe4439e919113f5a4b42a3621e5f2969b1b2
numanizudin <muhammadnumanizudin@gmail.com>
2022-03-08 10:04:43 +0700

    destination card

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:airplane/shared/theme.dart';
+import 'package:airplane/ui/widgets/destination_card.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -60,72 +61,42 @@ class HomePage extends StatelessWidget {
     Widget popularDestination() {
       return Container(
         margin: EdgeInsets.only(top: 30),
-        child: Row(
-          children: [
-            Container(
-              width: 200,
-              height: 323,
-              margin: EdgeInsets.only(left: defaultMargin),
-              padding: EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(18),
-                color: kWhiteColor,
+        child: SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            children: [
+              DestionationCard(
+                name: 'Lake Cipeles',
+                city: 'Sumedang',
+                imageUrl: 'assets/image_destination1.png',
+                rating: 4.8,
               ),
-              child: Column(
-                children: [
-                  Container(
-                    width: 180,
-                    height: 220,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(18),
-                      image: DecorationImage(
-                        image: AssetImage(
-                          'assets/image_destination1.png',
-                        ),
-                      ),
-                    ),
-                    child: Align(
-                      alignment: Alignment.topRight,
-                      child: Container(
-                        width: 55,
-                        height: 30,
-                        decoration: BoxDecoration(
-                          color: kWhiteColor,
-                          borderRadius: BorderRadius.only(
-                            bottomLeft: Radius.circular(18),
-                          ),
-                        ),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Container(
-                              height: 20,
-                              width: 20,
-                              margin: EdgeInsets.only(right: 2),
-                              decoration: BoxDecoration(
-                                image: DecorationImage(
-                                  image: AssetImage(
-                                    'assets/icon_star.png',
-                                  ),
-                                ),
-                              ),
-                            ),
-                            Text(
-                              '4.8',
-                              style: blackTextStyle.copyWith(
-                                fontWeight: medium,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
+              DestionationCard(
+                name: 'White Houses',
+                city: 'Spain',
+                imageUrl: 'assets/image_destination2.png',
+                rating: 4.7,
               ),
-            ),
-          ],
+              DestionationCard(
+                name: 'Hill Heyo',
+                city: 'Monaco',
+                imageUrl: 'assets/image_destination3.png',
+                rating: 4.8,
+              ),
+              DestionationCard(
+                name: 'Menarra',
+                city: 'Japan',
+                imageUrl: 'assets/image_destination4.png',
+                rating: 5.0,
+              ),
+              DestionationCard(
+                name: 'Payung Teduh',
+                city: 'Singapore',
+                imageUrl: 'assets/image_destination5.png',
+                rating: 4.8,
+              ),
+            ],
+          ),
         ),
       );
     }
